cc26f417506c0ec651cba94a57f8a30c3da4e4f7
Jonathan Pinto Barbosa <jonathanpbarbosa@gmail.com>
2017-09-05 21:30:45 +0000

    Paginate publications

--- publication/views.py
+++ publication/views.py
@@ -1,8 +1,20 @@
 from django.shortcuts import render
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from .models import Publication
 
 
 def index(request):
-    publications = Publication.objects.all()
+    publication_list = Publication.objects.all()
+    paginator = Paginator(publication_list, 10)
+    page = request.GET.get('page')
+    
+    try:
+        publications = paginator.page(page)
+    except PageNotAnInteger:
+        publications = paginator.page(1)
+    except EmptyPage:
+        publications = paginator.page(paginator.num_pages)
+        
     context = {'publications': publications}
+    
     return render(request, 'publication/index.html', context)
