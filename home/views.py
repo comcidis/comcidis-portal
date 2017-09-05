becd55a9579d0b55e27ed353aef1c2f9d1a0bd05
Jonathan Pinto Barbosa <jonathanpbarbosa@gmail.com>
2017-09-05 22:33:53 +0000

    Add recent publication in home page

--- home/views.py
+++ home/views.py
@@ -1,10 +1,7 @@
 from django.shortcuts import render
+from publication.models import Publication
 
 def index(request):
-    """Portal overview
-
-    :req: request
-    :returns: render page
-
-    """
-    return render(request, 'home/index.html')
+    recent_publications = Publication.objects.all()[:4]
+    context = {'recent_publications': recent_publications}
+    return render(request, 'home/index.html', context)
