ab34c67a384cadc0234adf6f1e47023c96cebcaa
Jonathan Pinto Barbosa <jonathanpbarbosa@gmail.com>
2017-09-14 17:09:31 +0000

    Add home sections

--- home/models.py
+++ home/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class Section(models.Model):
+    title = models.CharField(max_length=100)
+    short_description = models.CharField(max_length=175)
+    description = models.TextField()
+    
+    def __str__(self):
+        return self.title
--- home/views.py
+++ home/views.py
@@ -1,7 +1,15 @@
 from django.shortcuts import render
 from publication.models import Publication
+from member.models import LineOfResearch
+from .models import Section
 
 def index(request):
     recent_publications = Publication.objects.all()[:4]
-    context = {'recent_publications': recent_publications}
+    sections = Section.objects.all()
+    lines_of_research = LineOfResearch.objects.all()
+    half_of_list_length = round(len(lines_of_research) / 2)
+    research_columns = [lines_of_research[:half_of_list_length],
+                        lines_of_research[half_of_list_length:]]
+    context = {'recent_publications': recent_publications,
+               'sections': sections, 'research_columns': research_columns}
     return render(request, 'home/index.html', context)
